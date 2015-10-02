3cc352ded3235c5a46bc80699610dcd1f9476184
g-2 E989 <g2muon@fnal.gov>
2015-10-02 10:53:58 -0500

    Edited the path for a few startup scripts.

--- online/bin/start_frontends.sh
+++ online/bin/start_frontends.sh
@@ -4,7 +4,7 @@
 source $(dirname $(readlink -f $0))/../../common/.expt-env
 
 for fe in "${EXPT_FE[@]}"; do
-    fename="${EXPT_DIR}/online/frontends/${fe}"
+    fename="${EXPT_DIR}/common/bin/${fe}"
     scname="${EXPT}.${fe//_/-}"
     screen -dmS $scname
     screen -S $scname -p 0 -rX stuff "${fename} -e $EXPT$(printf \\r)"
