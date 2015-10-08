67ee4c77bcb950cd4dee749101b9f1d84359314b
Matthias W. Smith <mwsmith2@uw.edu>
2015-10-08 09:12:30 -0500

    Removed vestigial scripts.

--- online/frontends/mkfi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#screen -dmS test bash -c "echo \"bleh\" > tmp.txt"
-
-screen -dmS laser bash -c ".\run_laser.sh"
--- online/frontends/run_laser.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/cenpa/DAQ/frontends/nmr-frontends
-./laser_tracker -e gm2_nmr
-
