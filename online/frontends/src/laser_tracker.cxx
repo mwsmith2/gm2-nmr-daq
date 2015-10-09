2e127ce80b46e0872c37b4bb167e577144f95d9f
Matthias W. Smith <mwsmith2@uw.edu>
2015-10-09 15:20:08 -0500

    (Brendan) Modified laser tracker synchronization to use the lock files for all communication [deleting the lock file allows the Spatial Analyzer software to catch a trigger]

--- online/frontends/src/laser_tracker.cxx
+++ online/frontends/src/laser_tracker.cxx
@@ -16,9 +16,14 @@ About:  This frontend is synchorinzed to the SyncTrigger class in
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
+#include <string.h>
 #include <mutex>
+#include <iostream>
+#include <sstream>
+#include <string>
+
 using std::string;
+using namespace std;
 
 //--- other includes -----------------------------------------------//
 #include "midas.h"
@@ -70,6 +75,7 @@ extern "C" {
   INT interrupt_configure(INT cmd, INT source, PTYPE adr);
 
   void TriggerLaserTracker(float sleeptime, bool DIAG);
+  void ResetLaserTracker(int channel, float sleeptime, bool DIAG);
   
   int SetChannel(int channel, 
                  float newsetvalue, 
@@ -83,14 +89,14 @@ extern "C" {
   EQUIPMENT equipment[] = 
     {
       {FE_NAME, //"Laser Tracker", // equipment name
-       { 1, 0,          // event ID, trigger mask 
+       { 10, 0,          // event ID, trigger mask 
          "BUF2",      // event buffer (use to be SYSTEM)
          EQ_POLLED |
 	 EQ_EB,         // equipment type 
          0,             // not used 
          "MIDAS",       // format 
          TRUE,          // enabled 
-         RO_RUNNING, //| RO_ODB,   // read only when running 
+         RO_RUNNING| RO_ODB,   // read only when running 
          10,            // poll for 10ms 
          0,             // stop run after this event limit 
          0,             // number of sub events 
@@ -154,7 +160,8 @@ INT frontend_init()
 
   listener = new daq::SyncClient(trigger_addr, trigger_port);
   listenerStepper = new daq::SyncClient(trigger_addr, trigger_port+30);
-  // @sync: end boilderplate
+ 
+ // @sync: end boilderplate
   // Note that if no address is specifed the SyncClient operates
   // on localhost automatically.
 
@@ -176,6 +183,20 @@ INT frontend_exit()
 //--- Begin of Run --------------------------------------------------*/
 INT begin_of_run(INT run_number, char *error)
 {
+  // BK_TODO
+  // If lock file exists, delete it so that the laser tracker will trigger
+  static FILE* f;  
+  f = fopen ("/home/me/portal/Export2.txt", "r");
+  if (f !=NULL){
+    fclose(f);
+    // try to remove it                                                        
+    usleep(10000);
+    if( remove( "/home/me/portal/Export2.txt" ) != 0 )
+      perror( "Error deleting lock file" );
+    else
+      puts( "Begin of Run: Export2.txt Lock file successfully deleted" );
+  }
+
   // @sync: begin boilerplate
   listener->SetReady();
   listenerStepper->UnsetReady(); // don't move until we say so
@@ -243,55 +264,53 @@ INT poll_event(INT source, INT count, BOOL test) {
   static std::mutex m;
   if (listener->HasTrigger()) {
     
-    printf("Listener has a trigger\n");
+    printf("\t\t\t\t\tListener has a trigger\n");
+    
     // User: Issue trigger here.
     
     // In our case this means doing the set/reset sequence for the laser tracker hardware
-    TriggerLaserTracker(0.10, true); // 0.5 seconds, with diagnostics
+    
+    //Turning off for asynchronous mode
+    //    TriggerLaserTracker(3, true); // 10 seconds, make sure that scs sees it
+    //   ResetLaserTracker(2,3,true);  // need to be done with this sequence when the laser tracker is done
+    m.lock();
+    f = fopen ("/home/me/portal/Export2.txt", "r");
+    if (f !=NULL ){
+      fclose(f);
+      // try to remove it
+      usleep(10000);
+      
+      if (remove("/home/me/portal/Export2.txt") != 0) {
+	
+	perror( "Error deleting lock file" );
+	return 0;
 
-    f = fopen("/home/newg2/Applications/gm2-nmr/resources/laser-files/lock.txt","w");
-    fputs("sample",f);
-    fclose(f);
-    // temporarily create the lock file so we can check if it exists
-    // do this temp in the TriggerLaserTracker function
-  }
+      } else {
+	
+	puts( "Lock file successfully deleted" );
+      }
+      
+      m.unlock();
+      return 1;
+
+    } else {
+      
+      m.unlock();
+      return 1;
+    }
+    
+    m.unlock();
+    
+  } // if HasTrigger()
   
   // @sync: end boilerplate
   
   // In our case we need to now poll for the lock file
   // when it exists, it's time to readout the event
   // otherwise, just 
-  
-  m.lock();
-  f = fopen ("/home/newg2/Applications/gm2-nmr/resources/laser-files/lock.txt", "r");
-  if (f !=NULL){
-    fclose(f);
-    if( remove( "/home/newg2/Applications/gm2-nmr/resources/laser-files/lock.txt" ) != 0 )
-      perror( "Error deleting file" );
-    else
-      puts( "File successfully deleted" );
-    m.unlock();
-    return 1;
-  }
-  
-  m.unlock();
-  
+    
   return 0;
 
-  /*  
-  bool LockFileExists = true; // change to actually look for the lock file
-  static int counter = 0;
-  if (counter++ == 5000){ // poll one hundred times, then declare us ready
-    //LockFileExists){
-    // delete the LockFile
-    counter = 0;
-    return 1;
-    
-    
-  }
-  return 0;
-  */
-  //  return listener->HasTrigger(); // User: Check device for event here.
 }
 
 //--- Interrupt configuration ---------------------------------------*/
@@ -315,56 +334,118 @@ INT interrupt_configure(INT cmd, INT source, PTYPE adr)
 
 INT read_trigger_event(char *pevent, INT off)
 {
+  //  int count = 0;
+  char bk_name[10]; // Bank Name
+  double *pdata;    // Place to store data
   
-  int count = 0;
-  char bk_name[10];
-  double *pdata;
-  
-  // @user: readout routine here.
-  // And MIDAS output.
-  bk_init32(pevent);
-
+  // Initialize MIDAS BANK
+  bk_init32(pevent); 
   // Get the time as an example
   sprintf(bk_name, "LTRK");
   bk_create(pevent, bk_name, TID_DOUBLE, &pdata);
 
-  *(pdata++) = daq::systime_us() * 1.0e-6;
+  //  *(pdata++) = daq::systime_us() * 1.0e-6;
 
 
   // BEGIN Paul code
   
   HNDLE hDB;
   cm_get_experiment_database(&hDB, NULL);
-
+  static int count_cycles=0;
+  bool first = 1;
   
-  ////////////Testprgm///////////////
+  // ---  This is the stuff we need to move over ----- //
+  std::string::size_type sz;
+  FILE *ptr_file;
+  char line[1000];
+  char *til;
+  char *sp;
+
+  do {
+    ptr_file = fopen("/home/me/portal/Export2.txt", "r");
+    usleep(10000);
+  } while (ptr_file == NULL);
+
+  fclose(ptr_file);
+
+  // Open Data File (we have already checked for lock file)
+  ptr_file=fopen("/home/me/portal/Export1.txt","r"); 
+  if (!ptr_file) {
+    printf("Error when opening file\n");
+  }
+  
+  struct LaserLine {
+    double r;
+    double theta;
+    double z;
+    double t; // time
+  } ;
+  LaserLine dataLine[1]; // array of laserlines
+
+    // LOOP over valid lines in the file
+
+  int lineNumber =0;
+  while (fgets(line,1000,ptr_file)!=NULL) {
+    string sline(line);
+    stringstream ss(sline);
+    cout << "line is" << sline <<endl;    
+
+    int tokenNumber =0;    
+    string token;
+    while (getline(ss,token,','))
+      {
+	//	cout << "\t" << token << endl;
+	stringstream stream_token;
+	stream_token<<setprecision(10);
+	stream_token<<token;
+	
+	double value;
+	stream_token >> value;
+	cout <<value<<endl;
+	
+	// throw out the label and the time
+	switch (tokenNumber){
+	case 1:
+	  dataLine[lineNumber].r = value;
+	case 2:
+	  dataLine[lineNumber].theta = value;
+	case 3: 
+	  dataLine[lineNumber].z = value;
+	default:
+	    break;
+	} //switch case
+	++ tokenNumber;
+      } //loop over tokens
+    ++lineNumber;
+  }//loop over lines in the file
+
+
+   for (int i=0;i<lineNumber;++i)
+     {
+       cout << "r,theta,z "<<dataLine[i].r<<","<<dataLine[i].theta<<","<<dataLine[i].z<<endl;
+       *(pdata++)=dataLine[i].r;
+       *(pdata++)=dataLine[i].theta;
+       *(pdata++)=dataLine[i].z;
+       
+     }
+  
+  /*
+  //------------- CODE WE ARE REPLACING ----------//  
   FILE *ptr_file;
   double tilnum;
   char line[1000];
   char *til;
   char *sp;
   bool keepers[22] = {0,1,1,1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0};
-  //Check for lock file
-  //grep for file?
-  //potentially use [ -f file ]
-  //loop to keep checking
-  //copy file
-  //if file found, cp it
 
-
-  // Need new location on DAQ machine
-  //Utilize Matthias' light sleep here while looping and looking for the creation of a lock file.
-
+  
+  
   //  ptr_file=fopen("//media//sf_VBSF//Main_p2-Meas.txt","r"); //open lock file
-  ptr_file=fopen("/home/newg2/Applications/gm2-nmr/resources/laser-files/Main_p2-Meas.txt","r"); //open lock file
+  ptr_file=fopen("/home/me/portal/Export1.txt","r"); //open lock file
   if (!ptr_file) {
     printf("Error when opening file\n");
   }
-  //Delete lock file
-  //rm file
 
-  static int count_cycles=0;
-  bool first = 1;
 
   while (fgets(line,1000,ptr_file)!=NULL) {
     
@@ -414,17 +495,28 @@ INT read_trigger_event(char *pevent, INT off)
       } // (keepers index)
       til = strtok_r(NULL,",",&sp);
     }
-    if (first)
-	  {
-	    first=false;
-	    db_set_value(hDB, 0, "/Equipment/Laser Tracker/Variables/X", 
-			 &count_cycles, sizeof(count_cycles), 1, TID_INT); 
-	    count_cycles++;
-	  }
-  }
+
+  */
+
+  if (first)
+    {
+      first=false;
+      db_set_value(hDB, 0, "/Equipment/laser-tracker/Variables/X", 
+		   &count_cycles, sizeof(count_cycles), 1, TID_INT); 
+      count_cycles++;
+    }
+  
   fclose(ptr_file);
   
-  
+  if( remove( "/home/me/portal/Export1.txt" ) != 0 ) {
+    
+    perror( "Error deleting file" );
+
+  } else {
+    
+    puts( "File successfully deleted" );
+  }
+
   // End Paul code
   
 
@@ -434,8 +526,6 @@ INT read_trigger_event(char *pevent, INT off)
   // Let the steppertrigger listener know we are ready to move to the next
   printf("set listener stepper ready\n");
 
-  // TODO remove when syncronized
-  sleep(0.5); // protect against huge data rates
 
   listenerStepper->SetReady();
   listener->SetReady();
@@ -460,7 +550,7 @@ int SetChannel(int channel, float newsetvalue, HNDLE hDB, HNDLE hKey, float slee
 		      &newsetvalue, sizeof(newsetvalue), channel, TID_FLOAT, FALSE); 
    if (DIAG) {
      db_get_data(hDB, hKey, &output, &size, TID_FLOAT);
-     printf("[5] equals %f, [6] equals %f\n", output[5], output[6]);
+         printf("\t\t[1] equals %f, [2] equals %f\n", output[1], output[2]);
    }
    sleeptime *= 1000000.;
    usleep(sleeptime);
@@ -490,18 +580,43 @@ void TriggerLaserTracker(float sleeptime=1.0, bool DIAG=false){
     }
   }
   
-  int channel_set =5;  // The Set signal   channel number
-  int channel_reset =6;
+  int channel_set = 1;  // The Set signal   channel number
+  int channel_reset = 2;
   
   //send a Set Signal (Change channel 5 value to 1)
   SetChannel(channel_set,1.0, hDB, hKey, sleeptime, DIAG);
-  
-  SetChannel(channel_set,0.0, hDB, hKey, sleeptime, DIAG);
-  
-  SetChannel(channel_reset,1.0, hDB, hKey, sleeptime, DIAG);
-  
-  SetChannel(channel_reset,0.0, hDB, hKey,sleeptime, DIAG);
+  SetChannel(channel_set,0.0, hDB, hKey, 0.001, DIAG);
 
 
   // temporarily, create a lock file
+}
+
+void ResetLaserTracker(int channel = 2,float sleeptime=1.0, bool DIAG=false){
+  HNDLE hDB, hKey;
+  INT size;
+  float output[7];
+  int i = 0;
+  for(;i <7; ++i) {
+    output[i]=99;
+  }
+
+  cm_get_experiment_database(&hDB, NULL);
+  db_find_key(hDB, 0, "/Equipment/Environment/Variables/Output", &hKey);
+  size = sizeof(output);
+  db_get_data(hDB, hKey, &output, &size, TID_FLOAT);    
+  
+  if (DIAG)	{
+    //  printf("size is %d\n",size);
+    i = 0;
+    for(; i < 7; ++i) {
+      float tval = output[i];
+      //      printf("index %d = %f\n",i, tval);
+    }
+  }
+  
+  
+  //send a Set Signal (Change channel 5 value to 1)
+  SetChannel(channel,1.0, hDB, hKey, sleeptime, DIAG);
+  SetChannel(channel,0.0, hDB, hKey, 0.0001, DIAG);
+  
 }
