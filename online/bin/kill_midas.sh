151966dad8263fcfd51d6cb7ea4d9b1cd19b6cff
Matthias W. Smith <mwsmith2@uw.edu>
2015-10-22 15:47:05 -0500

    Updated midas kill script to kill all analyzers and frontends too.

--- online/bin/kill_midas.sh
+++ online/bin/kill_midas.sh
@@ -4,19 +4,7 @@
 source $(dirname $(readlink -f $0))/../../common/.expt-env
 
 # The script kills general midas utilies.
-for session in $(screen -ls | grep -o "[0-9]*\.${EXPT}.mhttpd")
-do screen -S "${session}" -p 0 -X quit
-done
-
-for session in $(screen -ls | grep -o "[0-9]*\.${EXPT}.mserver")
-do screen -S "${session}" -p 0 -X quit
-done
-
-for session in $(screen -ls | grep -o "[0-9]*\.${EXPT}.mlogger")
-do screen -S "${session}" -p 0 -X quit
-done
-
-for session in $(screen -ls | grep -o "[0-9]*\.${EXPT}.mevb")
+for session in $(screen -ls | grep -o "[0-9]*\.${EXPT}.*")
 do screen -S "${session}" -p 0 -X quit
 done
 
