fac12717305371adb4e366e14360886a926056d2
Matthias W. Smith <mwsmith2@uw.edu>
2015-10-22 12:13:39 -0500

    Fixed bug extracting laser_point

--- offline/src/extract_run_attr.py
+++ offline/src/extract_run_attr.py
@@ -12,61 +12,62 @@ def main():
     except:
         run_attr = {}
 
-    for run_num in range(800):
+    for run_num in range(850):
         run_files = glob.glob(arch_dir + '/*%05i*' % run_num)
-        run_attr[run_num] = {}
+        key = "%05i" % run_num
+        run_attr[key] = {}
         
         for f in run_files:
-            print f
 
             for line in open(f):
 
                 if 'xml' in f:
                     if '"step_size" type="DOUBLE"' in line:
                         step_size = float(line.split('>')[1].split('<')[0])
-                        run_attr[run_num]['step_size'] = step_size
+                        run_attr[key]['step_size'] = step_size
                             
                     if '"Laser Tracker Point" type="STRING"' in line:
-                        laser_point = line.split('>')[1].split('<')[0].upper()
 
-                        if laser_point != 'P1' or laser_point != 'P2':
+                        laser_point = line.split('>')[1].split('<')[0].upper()
+                        laser_point = laser_point.strip()
+
+                        if laser_point != 'P1' and laser_point != 'P2':
                             laser_point = 'N'
 
-                        run_attr[run_num]['laser_point'] = laser_point.strip()
+                        run_attr[key]['laser_point'] = laser_point
 
                 else:
 
                     if 'step_size = DOUBLE' in line:
                         step_size = float(line.split(':')[1])
-                        run_attr[run_num]['step_size'] = step_size
+                        run_attr[key]['step_size'] = step_size
                             
                     if 'Laser Tracker Point = STRING' in line:
-                        laser_point = line.split(':')[1].split(']')[1].upper()
 
-                        if laser_point != 'P1' or laser_point != 'P2':
+                        laser_point = line.split(':')[1].split(']')[1].upper()
+                        laser_point = laser_point.strip()
+
+                        if laser_point != 'P1' and laser_point != 'P2':
                             laser_point = 'N'
 
-                        run_attr[run_num]['laser_point'] = laser_point.strip()
-
-        print '\n'
-        print run_attr[run_num]
-        print '\n\n'
+                        run_attr[key]['laser_point'] = laser_point
 
         try:
-            run_attr[run_num]['step_size']
+            run_attr[key]['step_size']
 
         except:
-            run_attr[run_num]['step_size'] = -1.0
+            run_attr[key]['step_size'] = -1.0
 
         try:
-            run_attr[run_num]['laser_point']
+            run_attr[key]['laser_point']
 
         except:
-            run_attr[run_num]['laser_point'] = 'N'
+            run_attr[key]['laser_point'] = 'N'
 
+        print run_attr[key]
     
     with open(attr_file, 'w') as f:
-        f.write(json.dumps(run_attr, indent=2))
+        f.write(json.dumps(run_attr, indent=2, sort_keys=True))
 
 if __name__ == '__main__':
     main()
