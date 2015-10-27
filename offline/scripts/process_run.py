d90bf6f21455e89fe9a440e4f4687cb2f203dd03
Matthias W. Smith <mwsmith2@uw.edu>
2015-10-27 09:58:30 -0500

    Filled out data processing script to run from MIDAS.

---
+++ offline/scripts/process_run.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import time
+import os
+from subprocess import call
+
+import midas
+
+def main():
+    
+    offline_dir = os.path.dirname(__file__) + '/..'
+    os.chdir(offline_dir)
+
+    odb = midas.ODB('gm2-nmr')
+    run_num = odb.get_value("Runinfo/Run Number")
+
+    call(['python', 'scripts/test_crunch_runs.py', run_num])
+
+
+if __name__ == '__main__':
+    main()
+
