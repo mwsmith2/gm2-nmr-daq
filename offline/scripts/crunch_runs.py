a4a8cd1805c2f04de971c03ab2d7822ff74793cf
Matthias W. Smith <mwsmith2@uw.edu>
2015-10-28 10:35:26 -0500

    Tracks checksums and safer for multiple crunching threads.

--- offline/scripts/crunch_runs.py
+++ offline/scripts/crunch_runs.py
@@ -1,3 +1,5 @@
+#!/bin/python
+
 import os
 import sys
 import json
@@ -5,24 +7,12 @@ import glob
 import subprocess
 import hashlib
 
-info = {}
 
 def main():
 
     offline_dir = os.path.dirname(__file__) + '/..'
     os.chdir(offline_dir)
     
-    data_info_file = 'data/processing_info.json'
-    global info
-
-    try:
-        info = json.loads(open(data_info_files))
-    
-    except:
-        info = { 'runs': {}, 'crunched_files': [] }
-
-    print info
-
     runs = []
 
     if len(sys.argv) > 1:
@@ -30,54 +20,72 @@ def main():
             runs.append(int(i))
 
     else:
+        
+        info_file = 'data/crunched/.processing_metadata.json'
+        info = json.loads(open(info_file).read())
 
-        for mf in glob.glob('data/midas/*mid'):
-            print mf
+        try:
+            run_num = info['last_full_crunch'] + 1
 
-            if mf in info['crunched_files']:
-                pass
+        except:
+            info['last_full_crunch'] = 1
+            run_num = 1
 
-            else:
-                run_num = int(mf.split('.')[-2][-5:])
-                process_midas(run_num)
+        while True:
+            
+            if run_num > info['last_full_crunch']:
+                last_full_crunch = run_num
 
-        for rf in glob.glob('data/rome/*.root'):
-            print rf
-
-            if rf in info['crunched_files']:
-                pass
-
-            else:
-                run_num = int(rf.split('.')[-2][-5:])
-                process_root(run_num)
-
-        for rf in glob.glob('data/shim/*.root'):
-            print rf
-
-            if rf in info['crunched_files']:
-                pass
+            if len(glob.glob('data/midas/run%05i.mid' % run_num)) > 0:
+                runs.append(run_num)
+                run_num += 1
 
             else:
-                run_num = int(rf.split('.')[-2][-5:])
-                process_shim(run_num)
+                break
+
+        info = json.loads(open(info_file).read())
+
+        info['last_full_crunch'] = last_full_crunch
+
+        info = open(info_file, 'w').write(json.dumps(info))
+
+    runs.sort()
 
     for run in runs:
-        process_midas(run)
-        process_root(run)
-        process_shim(run)
+        try:
+            process_midas(run)
+            process_root(run)
+            process_shim(run)
+        except:
+            pass
 
-    print os.getcwd()
-    with open(data_info_file, 'w') as f:
-        f.write(json.dumps(info))
 
 def process_midas(run_num):
     print "process_midas(%i)" % run_num
-    
+
+    info_file = 'data/rome/.processing_metadata.json'
+    run_key = str(run_num)
+
     if (run_num < 787):
         rome_analyzers = ['sync', 'ctec', 'slowcont', 'tilt']
 
     else:
         rome_analyzers = ['sync_ext_ltrk', 'ctec', 'slowcont', 'tilt']
+
+    try:
+        info = json.loads(open(info_file).read())[run_key]
+
+    except:
+        # Initialize the metadata.
+        info = {}
+
+    for ra in rome_analyzers:
+
+        try:
+           info[ra] == None
+
+        except:
+           info[ra] = 0
 
     cmd_prefix = "./midanalyzer.exe -i romeConfig.xml -r "
     cmd_suffix = " -m offline -p 0 -q"
@@ -85,61 +93,138 @@ def process_midas(run_num):
     for ra in rome_analyzers:
 
         os.chdir('rome-analyzers/' + ra)
-        print cmd_prefix + str(run_num) + cmd_suffix
-        subprocess.call(cmd_prefix + str(run_num) + cmd_suffix, shell=True)
 
-        for rf in glob.glob('*.root'):
-            subprocess.call(['rm', rf])
+        md5sum = hashlib.md5(open('midanalyzer.exe', 'r').read()).hexdigest()
 
-        os.chdir('../../')
+        if info[ra] == md5sum:
+            print "Already processed for %s." % ra
+            os.chdir('../../')
+
+        else:
+            subprocess.call(cmd_prefix + str(run_num) + cmd_suffix, shell=True)
+            info[ra] = md5sum
+
+            # Clean up unecessary files.
+            for rf in glob.glob('*.root'):
+                subprocess.call(['rm', rf])
         
-    try:
-        info[run_num]['rome'] = rome_analyzers
-    
-    except:
-        info[run_num] = { 'rome': rome_analyzers }
+            # Back to offline dir
+            os.chdir('../../')
 
-    for f in glob.glob('data/midas/*%i.mid' % run_num):
-        info['crunched_files'].append(f)
+    all_info = json.loads(open(info_file).read())
+    all_info[run_key] = info
+    with open(info_file, 'w') as f:
+        f.write(json.dumps(all_info, indent=2, sort_keys=True))
 
 
 def process_root(run_num):
     print "process_root(%i)" % run_num
 
+    info_file = 'data/shim/.processing_metatdata.json'
+    run_key = str(run_num)
     cmd = 'bin/shim_data_bundler'
-    subprocess.call([cmd, str(run_num)])
+
+    try:
+        info = json.loads(open(info_file).read())[run_key]
+
+    except:
+        # Initialize the metadata.
+        info = {}
+
+    try:
+        info[cmd] == None
+
+    except:
+        info[cmd] = 0
+
+    input_files = []
+    for f in glob.glob('data/rome/%05i.root' % run_num):
+        input_files.append(f)
+
+    for f in glob.glob('data/root/%05i.root' % run_num):
+        input_files.append(f)
+
+    # Make sure none of the input files have changed.
+    for f in input_files:
+
+        md5sum = hashlib.md5(open(f, 'r').read()).hexdigest()
+        
+        try:
+            if info[f] == md5sum:
+                pass
+
+            else:
+                # Reset the processing
+                info[fname] = md5sum        
+                info[cmd] = 0
+
+        except:
+            info[f] = md5sum
 
     md5sum = hashlib.md5(open(cmd, 'r').read()).hexdigest()
 
-    try:
-        info[run_num]['shim'] = md5sum
-    
-    except:
-        info[run_num] = { 'shim': md5sum }
+    if info[cmd] == md5sum:
+        print "Already processed with %s." % cmd
+        return
 
-    for f in glob.glob('data/root/*%i.root' % run_num):
-        info['crunched_files'].append(f)
+    else:
+        subprocess.call([cmd, str(run_num)])
+        info[cmd] = md5sum
 
-    for f in glob.glob('data/rome/*%i.root' % run_num):
-        info['crunched_files'].append(f)
-
+    all_info = json.loads(open(info_file).read())
+    all_info[run_key] = info
+    with open(info_file, 'w') as f:
+        f.write(json.dumps(all_info, indent=2, sort_keys=True))
 
 def process_shim(run_num):
     print "process_shim(%i)" % run_num
 
+    info_file = 'data/crunched/.processing_metadata.json'
+    run_key = str(run_num)
     cmd = './bin/recrunch_fids'
-    subprocess.call([cmd, 'data/shim/run_%05i.root' % (run_num)])
-    
-    md5sum = hashlib.md5(open(cmd, 'r').read()).hexdigest()
 
     try:
-        info[run_num]['crunched'] = md5sum
-    
-    except:
-        info[run_num] = { 'crunched': md5sum }
+        info = json.loads(open(info_file).read())[run_key]
 
-    for f in glob.glob('data/shim/*%i.root' % run_num):
-        info['crunched_files'].append(f)
+    except:
+        # Initialize the metadata.
+        info = {}
+
+    try:
+        info[cmd] == None
+
+    except:
+        info[cmd] = 0
+
+    # Check to see if the input file has changed.
+    fname = 'data/shim/run_%05i.root' % run_num
+
+    md5sum = hashlib.md5(open(fname, 'r').read()).hexdigest()
+    try:
+        if info[fname] == md5sum:
+            pass
+
+        else:
+            info[fname] = md5sum        
+            info[cmd] = 0
+
+    except:
+        info[fname] = md5sum        
+ 
+    md5sum = hashlib.md5(open(cmd, 'r').read()).hexdigest()
+
+    if info[cmd] == md5sum:
+        print "Already processed with %s." % cmd
+        return
+
+    else:
+        subprocess.call([cmd, 'data/shim/run_%05i.root' % (run_num)])
+        info[cmd] = md5sum
+    
+    all_info = json.loads(open(info_file).read())
+    all_info[run_key] = info
+    with open(info_file, 'w') as f:
+        f.write(json.dumps(all_info, indent=2, sort_keys=True))
 
 if __name__ == '__main__':
     main()
