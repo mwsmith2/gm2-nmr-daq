e23b14370c256a25dcaf7f6a4070e128800c62a5
Matthias W. Smith <mwsmith2@uw.edu>
2015-10-28 10:36:12 -0500

    Smart enough to not process run in progress now.

--- offline/scripts/process_run.py
+++ offline/scripts/process_run.py
@@ -12,10 +12,14 @@ def main():
     os.chdir(offline_dir)
 
     odb = midas.ODB('gm2-nmr')
-    run_num = odb.get_value("Runinfo/Run Number")
+    run_num = int(odb.get_value("Runinfo/Run Number"))
+    run_state = int(odb.get_value("Runinfo/State"))
 
-    call(['python', 'scripts/crunch_runs.py', run_num])
+    if run_state == 1:
+        call(['python', 'scripts/crunch_runs.py', str(run_num)])
 
+    else:
+        call(['python', 'scripts/crunch_runs.py', str(run_num - 1)])
 
 if __name__ == '__main__':
     main()
