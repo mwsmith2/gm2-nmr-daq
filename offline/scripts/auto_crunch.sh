0656139b0b00c8e0780487e7bf6444a4a75bb5d4
Matthias W. Smith <mwsmith2@uw.edu>
2015-11-30 09:15:10 -0600

    Updated to run script in bg only if not running.

--- offline/scripts/auto_crunch.sh
+++ offline/scripts/auto_crunch.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 source /home/newg2/.bashrc
+source /home/newg2/.shrc
 
 cd $(dirname $(readlink -f $0))/..
-python scripts/crunch_runs.py
+
+if [ "$(pidof g2field-crunchd)" ]
+then
+    echo "g2field-crunchd is running."
+else
+    echo "Restarting g2field-crunchd."
+    python scripts/g2field-crunchd.py
+fi
 
 # end script
