bb7af41a7720c2fddbd93337e36b7302c23a7ef9
Matthias W. Smith <mwsmith2@uw.edu>
2015-11-30 09:21:56 -0600

    Added crunchd start script, removed auto crunch.

---
+++ offline/scripts/check_crunchd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source /home/newg2/.bashrc
+source /home/newg2/.shrc
+
+cd $(dirname $(readlink -f $0))/..
+
+if [ "$(pidof gm2-nmr-crunchd)" ]
+then
+    echo "gm2-nmr-crunchd is running."
+else
+    echo "Restarting gm2-nmr-crunchd."
+    python scripts/gm2-nmr-crunchd.py &>> ${1} &
+fi
+
+# end script
