5f28fc57db3f9ec348ca43e8bcbe30451fd199b6
Matthias W. Smith <mwsmith2@uw.edu>
2015-12-01 09:43:54 -0600

    Added test for requesting crunchd info.

---
+++ offline/scripts/test_request.py
@@ -0,0 +1,23 @@
+import sys
+import time
+import zmq
+import json
+
+def main():
+    ctx = zmq.Context()
+    sck = ctx.socket(zmq.REQ)
+    sck.connect('tcp://localhost:44446')
+    time.sleep(0.01)
+
+    info = {}
+    info['type'] = 'check'
+    info['body'] = {}
+    info['body']['type'] = 'normal'
+    info['body']['msg'] = {'run': int(sys.argv[1]), 'type':'normal'}
+
+    rc = sck.send(json.dumps(info))
+    print json.dumps(sck.recv_json(), indent=2)
+
+if __name__ == '__main__':
+    main()
+
