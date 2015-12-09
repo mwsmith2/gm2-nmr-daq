b7a2cee492b5bcb472b8d5769acbe2c9f157313d
Matthias W. Smith <mwsmith2@uw.edu>
2015-12-08 23:50:59 -0600

    A simple script to backup json metadata.

--- offline/scripts/backup_metadata.py
+++ offline/scripts/backup_metadata.py
@@ -2,27 +2,31 @@
 
 import glob
 import json
+import midas
 
 def main():
 
     # Get the data directory
     odb = midas.ODB('gm2-nmr')
     datadir = odb.get_value('/Logger/Data dir').rstrip() + '/'
-
     metafiles = glob.glob(datadir + '/*/.processing_metadata.json')
 
     for metafile in metafiles:
         
         with open(metafile) as f:
             mf_string = f.read()
-
+        
         try:
             # Load the json file.
             metadata = json.loads(mf_string)
 
             # Save it if not corrupted.
             with open(metafile + '.old', 'w') as f:
-                json.dumps(metadata, f)
+                f.write(json.dumps(metadata))
         
         except:
             pass
+
+
+if __name__ == '__main__':
+    main()
