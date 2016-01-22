8739442f4a4b78d43d749ce2118d27a1d664f6ba
Matthias W. Smith <mwsmith2@uw.edu>
2016-01-22 15:10:12 -0600

    Updates for new version of libfid (0.7.0).

--- online/frontends/src/shim_platform_full_wf.cxx
+++ online/frontends/src/shim_platform_full_wf.cxx
@@ -526,7 +526,7 @@ INT read_platform_event(char *pevent, INT off)
       data.trace[idx][n] = wf[n];
     }
 
-    fid::FID myfid(tm, wf);
+    fid::Fid myfid(tm, wf);
     
     // Make sure we got an FID signal
     if (myfid.isgood()) {
@@ -538,7 +538,7 @@ INT read_platform_event(char *pevent, INT off)
       
     } else {
       
-      myfid.PrintDiagnosticInfo();
+      myfid.DiagnosticInfo();
       data.freq[idx] = -1.0;
       data.ferr[idx] = -1.0;
       data.snr[idx] = -1.0;
