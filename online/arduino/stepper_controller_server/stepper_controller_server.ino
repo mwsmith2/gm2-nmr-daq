f12cdd374e408a14bbf34e2d1ca14a11a9fcd0c3
Matthias W. Smith <mwsmith2@uw.edu>
2016-05-30 09:41:57 -0500

    First pass at cleaning up arduino folder.

--- online/arduino/stepper_controller_server/stepper_controller_server.ino
+++ online/arduino/stepper_controller_server/stepper_controller_server.ino
@@ -1,5 +1,5 @@
-/*
-  Web server
+/*****************************************************************************\
+ Web server
 
  This sketch makes the Arduino Ethernet shield into a server that 
  listens for a daq computer to connect through ethernet TCP socket 
@@ -11,44 +11,41 @@
  tutorials modified
  by Ronaldo Ortez
  2 Jul 2015
+\*****************************************************************************/
 
-*/
+//-- std includes -----------------------------------------------------------//
 #include <stdlib.h> 
+
+//-- arduino includes -------------------------------------------------------//
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-
 #include <SPI.h>
 #include <Ethernet.h>
-
 #include <utility/w5100.h>
 #include <utility/socket.h>
-//#include <util.h>
 
-// network configuration-------------------------------------------------------
+//--- network configuration -------------------------------------------------//
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+// Set MAC address for your controller below (printed on shield.
 uint8_t mac[] = {0x90, 0xA2, 0xDA, 0x0F, 0x6D, 0xFA};
 
-byte client_ip[]= {192, 168, 1, 100};  // numeric IP for MIDAS front-end
+// Set numeric IP for MIDAS front-end.
+byte client_ip[]= {192, 168, 1, 100};  
 
 // Set the static IP address to use if the DHCP fails to assign
-uint8_t s_ip[4]={192, 168, 1, 140};
-uint8_t s_gateway[4]={10, 95, 100, 19};
+uint8_t s_ip[4] = {192, 168, 1, 140};
+uint8_t s_gateway[4] = {192, 168, 1, 1};
 uint8_t s_subnet[4] = {255, 255, 255, 0};
 
-// use port 80 to communicate
-//EthernetServer server = EthernetServer(80);
-
-//Our socket 0 will be opened in raw mode
+// Our socket 0 will be opened in raw mode.
 SOCKET s;
 
-//define receive buffer parameters
+// Define receive buffer parameters.
 byte rbuf[10];//receive buffer
 int rbuflen; //receive buffer length
 byte cbuf[1];//confirm buffer 
 
-//Sepper Motor configuration-------------------------------------------------
+//--- Sepper Motor configuration --------------------------------------------//
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
@@ -62,8 +59,8 @@ void setup()
   AFMS.begin(110);
   myMotor->setSpeed(1); // 1rpm
 
-  //Start the serial output
-  Serial.begin(115200);//this is the highest baud for the Ethernet shield
+  //Start the serial output, argument is baud_rate;
+  Serial.begin(115200);
 
   Serial.println("Initializing W5100 chip and TCP socket");
   W5100.init();
@@ -72,13 +69,12 @@ void setup()
   // Assign network properties to chip.
   W5100.setMACAddress(mac);
   W5100.setIPAddress(s_ip);
-  W5100.writeSnPORT(s, 5555); //set port to 80
+  W5100.writeSnPORT(s, 5555);
   W5100.setSubnetMask(s_subnet);
   W5100.setGatewayIp(s_gateway);
 
- // W5100.execCmdSn(s, Sock_OPEN);
-  
-  //W5100.execCmdSn(s, Sock_LISTEN);
+  // W5100.execCmdSn(s, Sock_OPEN);
+  // W5100.execCmdSn(s, Sock_LISTEN);
 
   
 }
@@ -88,25 +84,30 @@ void loop()
   Serial.println("Opening socet and listen for connections");
   
   W5100.execCmdSn(s, Sock_OPEN);
-  W5100.execCmdSn(s,Sock_LISTEN);
+  W5100.execCmdSn(s, Sock_LISTEN);
   uint8_t status = W5100.readSnSR(0);
   int count = 1;
 
  // check Status Register to find out if the connection was established
- // while ( (W5100.readSnSR(s) & SnSR:: ESTABLISHED) == SnSR::ESTABLISHED){
- while(W5100.readSnSR(0)==20) {
-      delay(1);
-      if (count ==2) Serial.println("Listening for a connection...");
+ while (W5100.readSnSR(0) == 20) {
+      delay(1);           
+      if (count == 2) {
+        Serial.println("Listening for a connection...");
+      }
       count++;
  }
 
+ // Reset count.
  count = 1; 
- // wait for client to connect
- while ( W5100.readSnSR(0) ==23) {
-     if( count <2) Serial.println("Connection established! What is the daq sending?");
+
+ // Wait for client to connect
+ while (W5100.readSnSR(0) == 23) {
+     if (count < 2) {
+        Serial.println("Connection established! What is the daq sending?");
+     }
 
      // wait for client message
-     while ((rbuflen = W5100.getRXReceivedSize(s))>0) {
+     while ((rbuflen = W5100.getRXReceivedSize(s)) > 0) {
 
         Serial.println("Incoming packet detected"); 
         W5100.recv_data_processing(s, rbuf, rbuflen);
@@ -123,28 +124,29 @@ void loop()
         Serial.print("Steps = ");
         Serial.println(steps);
 
-        if(steps>0) {
+        if (steps > 0) {
           Serial.print("Moving the stepper motor ");
           Serial.print(steps);
           Serial.println(" steps Forward");
-          myMotor->step(steps,FORWARD, SINGLE);
+          myMotor->step(steps, FORWARD, SINGLE);
           
-        } else if (steps<0) {
+        } else if (steps < 0) {
           Serial.print("Moving stepper motor ");
           Serial.print(-1*steps);
           Serial.println(" steps Backward");
-          myMotor->step(-1*steps,BACKWARD, SINGLE);
+          myMotor->step(-1*steps, BACKWARD, SINGLE);
           
         }else {
           Serial.println("Instrunctions not interpretted");
         }
+
         // Reset the receive bit and zero receive buffer
-        W5100.writeSnIR(s,0x4);
-        for (int i = 0; i< sizeof(rbuf);i++) {
-            rbuf[i] = (char ) 0;
+        W5100.writeSnIR(s, 0x4);
+        for (int i = 0; i< sizeof(rbuf); i++) {
+            rbuf[i] = '0';
         }
         
-        //now lets send a confirmation message to the daq computer
+        // Now lets send a confirmation message to the daq computer.
         Serial.println("Stepper motor finished, ready for next command");
         uint8_t cmsg = 1;
         uint8_t* tmsg = &cmsg;
@@ -154,7 +156,7 @@ void loop()
         W5100.execCmdSn(s, Sock_SEND);
         
         uint8_t trans = W5100.getTXFreeSize(s);
-        if(trans<1) {
+        if (trans < 1) {
           Serial.println("Could not send feedback");
         } else{
           Serial.println("Feedback sent");
@@ -164,38 +166,47 @@ void loop()
         //Lets flush the serial buffer to make sure nothing is left.
         Serial.flush();
         
-      }//end connection
-      if (count % 5==0)  Serial.println("5 loops in listening");
-  }//end open and listening
+      } // end connection
+
+      if ((count % 5) == 0) {
+          Serial.println("5 loops in listening");
+      }
+  } // end open and listening
   
-  //release the motor
+  // Release the motor.
   myMotor->release();
   
-  //close the socket
+  // Close the socket
   Serial.println("Closing socket now");
   W5100.execCmdSn(s, Sock_CLOSE); 
-}//loop
+} // loop
 
 byte socketStat[MAX_SOCK_NUM];
 
 void ShowSockStatus()
 {
   for (int i = 0; i < MAX_SOCK_NUM; i++) {
+
     Serial.print(F("Socket#"));
     Serial.print(i);
+
     uint8_t s = W5100.readSnSR(i);
     socketStat[i] = s;
+
     Serial.print(F(":0x"));
-    Serial.print(s,16);
+    Serial.print(s, 16);
     Serial.print(F(" "));
     Serial.print(W5100.readSnPORT(i));
     Serial.print(F(" D:"));
+
     uint8_t dip[4];
     W5100.readSnDIPR(i, dip);
-    for (int j=0; j<4; j++) {
-      Serial.print(dip[j],10);
-      if (j<3) Serial.print(".");
+ 
+   for (int j = 0; j < 4; j++) {
+      Serial.print(dip[j], 10);
+      if (j < 3) Serial.print(".");
     }
+
     Serial.print(F("("));
     Serial.print(W5100.readSnDPORT(i));
     Serial.println(F(")"));
